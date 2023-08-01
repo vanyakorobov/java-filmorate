585f98536ed5850461fbc0e8da663af901d09c6a
Ivan <vanya.korobov@yandex.ru>
2023-08-01 19:29:18 +0300

    Первая проверка.

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate.storage;
-
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface FilmStorage {
