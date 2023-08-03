e516ebe555fff04208f0902a656f490cd6fe1eba
Ivan <vanya.korobov@yandex.ru>
2023-08-03 16:12:43 +0300

    Первая проверка.

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class UserService {
